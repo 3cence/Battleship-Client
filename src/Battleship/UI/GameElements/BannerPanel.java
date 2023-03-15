655b34bf17dba9e9e799a096077a7ec0772c323b
Emma <emmaalderson24@gmail.com>
2023-03-15 14:23:32 -0400

    disabled debug pink

--- src/Battleship/UI/GameElements/BannerPanel.java
+++ src/Battleship/UI/GameElements/BannerPanel.java
@@ -10,7 +10,7 @@ public class BannerPanel extends JPanel {
     @Override
     protected void paintChildren(Graphics g) {
         super.paintChildren(g);
-        g.setColor(Color.pink);
-        g.fillRect(10, 10, 1600, 400);
+//        g.setColor(Color.pink);
+//        g.fillRect(10, 10, 1600, 400);
     }
 }
